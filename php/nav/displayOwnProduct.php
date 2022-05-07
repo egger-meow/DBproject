4c9a1325a21e261f73b1306808849370dcfab60a
egger-meow <9704@mail.cdps.hc.edu.tw>
2022-05-07 15:21:44 +0800

    uploadProduct_Bug

--- php/nav/displayOwnProduct.php
+++ php/nav/displayOwnProduct.php
@@ -14,10 +14,73 @@ session_start();
   try{
     $s=$conn->prepare("select * from products where SID =:sid");
     $s->execute(array('Acc' => $$_SESSION['curUser']['SID']));
+    $count = 1;
     foreach($s->fetchAll() as $product){
 
+      $PID = $product['PID'];
+      $productName = $product['name'];
+      $price = $product['price'];
+      $quantity	 = $product['quantity'];
 
+      $sql="select * FROM productimage WHERE PID=$PID";
+      $result = $conn->query($sql);
+      $conn->close();
+      
+      //查詢結果
+      $imageDisplay = "";
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          $img=$row['image'];
+          $logodata = $img;
+          $imageDisplay = '<img  with="50" heigh="10" src="data:'.$row['imgType'].';base64,' . $logodata . '" />';
+        }
+      }
 
+      echo <<<EOT
+        <tr>
+            <th scope="row">$count</th>
+            <td>$imageDisplay</td>
+            <td>$productName</td>
+
+            <td>$price </td>
+            <td>$quantity </td>
+            <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#$productName-1">
+                    Edit
+                </button></td>
+            <!-- Modal -->
+            <div class="modal fade" id="$productName-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">$productName Edit</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <label for="ex71">price</label>
+                                    <input class="form-control" id="ex71" type="text">
+                                </div>
+                                <div class="col-xs-6">
+                                    <label for="ex41">quantity</label>
+                                    <input class="form-control" id="ex41" type="text">
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <td><button type="button" class="btn btn-danger">Delete</button></td>
+        </tr>
+      EOT;
+      $count ++;
     }
 
    
@@ -46,48 +109,7 @@ session_start();
 ?>
 
 
-                            <tr>
-                                <th scope="row">1</th>
-                                <td><img src="Picture/1.jpg" with="50" heigh="10" alt="Hamburger"></td>
-                                <td>Hamburger</td>
-
-                                <td>80 </td>
-                                <td>20 </td>
-                                <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#Hamburger-1">
-                                        Edit
-                                    </button></td>
-                                <!-- Modal -->
-                                <div class="modal fade" id="Hamburger-1" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="staticBackdropLabel">Hamburger Edit</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    <div class="col-xs-6">
-                                                        <label for="ex71">price</label>
-                                                        <input class="form-control" id="ex71" type="text">
-                                                    </div>
-                                                    <div class="col-xs-6">
-                                                        <label for="ex41">quantity</label>
-                                                        <input class="form-control" id="ex41" type="text">
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Edit</button>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <td><button type="button" class="btn btn-danger">Delete</button></td>
-                            </tr>
+                            
                             <tr>
                                 <th scope="row">2</th>
                                 <td><img src="Picture/2.jpg" with="10" heigh="10" alt="coffee"></td>
