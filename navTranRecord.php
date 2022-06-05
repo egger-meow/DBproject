e96aa2c9fcf24ccd42320a8490447c00d79a5f13
egger-meow <9704@mail.cdps.hc.edu.tw>
2022-06-05 20:10:23 +0800

    houPartDone(basic)2

--- navTranRecord.php
+++ navTranRecord.php
@@ -2,16 +2,27 @@
 
 <?php
 
-session_start();
-require "php/shit/head2.php";
+  session_start();
+  require "php/shit/head2.php";
 
-if(!$_SESSION['Authenticated']){
-  header('Location: index.php');
-} 
+  if(!$_SESSION['Authenticated']){
+    header('Location: index.php');
+  } 
 
-require "php/shit/dbConnect.php";
+  require "php/shit/dbConnect.php";
+  $action = "";
+  $fuck = "all" ;
 
+  if (isset($_POST['ac'])) {
+    $fuck = $_POST['ac'] == "" ? "all" : $_POST['ac'] ;
+    if ($_POST['ac'] != "all") {
+      $action = " where TransactionType = '" . $_POST['ac']."'";
+    } else {
+      $action = "";
+    }
+  }
 ?>
+
 
 <nav class="navbar navbar-inverse">
 <div class="container-fluid">
@@ -43,8 +54,20 @@ require "php/shit/dbConnect.php";
   </ul>
 
   <div class="tab-content">   
+    
     <div id="menu1" class="tab-pane fade in active">
+    
       <div class="row">
+        <hr>
+      <label class="control-label col-sm-1" for="distance">action</label>
+            <div class="col-sm-5">  
+              <select name="distance" class="form-control" id="action">
+                <option value = "all"     >all</option>
+                <option value = "receive" >receive</option>
+                <option value = "payment" >payment </option>
+                <option value = "recharge">recharge</option>
+              </select>
+            </div>
         <div class="  col-xs-8">
           <table class="table" style=" margin-top: 15px;">
 
@@ -62,14 +85,16 @@ require "php/shit/dbConnect.php";
             <tbody>
 
 <?php
-  foreach ($conn -> query('select * from transactions') as $id => $tra) {
+  foreach ($conn -> query("select * from transactions" . $action) as $id => $tra) {
+    
+
     $UID    = $tra['UID']; 
 
     $stmt   = $tra['TransactionType'] == 'receive' ? $conn->prepare("select shopName from shops where UID = $UID;") : $conn->prepare("select account from users where UID = $UID");
     $stmt -> execute();
 
     $trader = $stmt->fetch()[0];
-    $sign   = $tra['TransactionType'] == 'payment' ? '-' : '+'
+    $sign   = $tra['TransactionType'] == 'payment' ? '-' : '+';
 ?> 
 
             <tr>
@@ -78,7 +103,7 @@ require "php/shit/dbConnect.php";
               <td><?=$tra['timeTransaction']?></td>
               <td><?=$trader?></td>
               <td><?=$sign?><?=$tra['TransactionAmount']?></td> 
-            </tr>
+            </tr> 
 
 <?php
   }
@@ -92,7 +117,13 @@ require "php/shit/dbConnect.php";
   </div>
 </div>
 
+<form action="navTranRecord.php" id="pp"  method="post" >
+  <input type="hidden" id="ac" name="ac" value="3487">
+</form>
+
 <script>
+
+  $('#action option[value=<?=$fuck?>]').prop('selected', true);
 
   $('li.dropdown').mouseover(function () {
 
@@ -104,6 +135,17 @@ require "php/shit/dbConnect.php";
       $(this).removeClass('open');
   });
 
+  $(document).ready(function() {
+
+    $("#action").change(function(){
+      var value =  $("#action option:selected")
+      
+      $('#ac').attr('value', value.text());
+      $( "#pp" ).submit();
+      
+    });
+
+  });
 </script>
 
 
